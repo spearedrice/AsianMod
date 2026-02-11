f8f45d55a38daa2fef5a51c3363698e56b03cf7f
Rice <spearedrice@gmail.com>
2026-02-10 21:24:23 -0500

    The beginning of something great.

---
+++ src/main/java/org/spearedrice/asianmod/AsianMod.java
@@ -0,0 +1,16 @@
+package org.spearedrice.asianmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AsianMod implements ModInitializer {
+	public static final String MOD_ID = "asianmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// die
+	}
+}
---
+++ src/main/java/org/spearedrice/asianmod/AsianModClient.java
@@ -0,0 +1,11 @@
+package org.spearedrice.asianmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AsianModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
