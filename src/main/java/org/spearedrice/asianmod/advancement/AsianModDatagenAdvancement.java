41d75ed7ba2cd42fbf77f3337a64699792062eb3
spearedrice <225486530+spearedrice@users.noreply.github.com>
2026-05-08 19:00:23 -0400

    fix one thing

--- src/main/java/org/spearedrice/asianmod/advancement/AsianModDatagenAdvancement.java
+++ src/main/java/org/spearedrice/asianmod/advancement/AsianModDatagenAdvancement.java
@@ -10,28 +10,27 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 
 public class AsianModDatagenAdvancement implements ModInitializer {
-
-    private static final HashMap<Item, Integer> TOOL_USES = new HashMap<>();
-
     @Override
     public void onInitialize() {
         ModCriteria.init();
 
-        PlayerBlockBreakEvents.AFTER.register((level, player, pos, state, blockEntity) -> {
-            if (player instanceof ServerPlayer serverPlayer) {
+        HashMap<Item, Integer> tools = new HashMap<>();
 
+        PlayerBlockBreakEvents.AFTER.register(((level, player, blockPos, blockState, blockEntity) -> {
+            if (player instanceof ServerPlayer serverPlayer) {
                 Item item = player.getMainHandItem().getItem();
 
-                int usedCount = TOOL_USES.getOrDefault(item, 0) + 1;
-                TOOL_USES.put(item, usedCount);
+                Integer usedCount = tools.getOrDefault(item, 0);
+                usedCount++;
+                tools.put(item, usedCount);
 
                 ModCriteria.USE_TOOL.trigger(serverPlayer);
                 ModCriteria.PARAMETERIZED_USE_TOOL.trigger(serverPlayer, usedCount);
 
                 serverPlayer.sendSystemMessage(
-                        Component.literal("Tool used: " + item.getDescription().getString() + " (" + usedCount + ")")
+                        Component.nullToEmpty("You've used \"" + item + "\" as a tool " + usedCount + " times!")
                 );
             }
-        });
+        }));
     }
 }
--- src/main/java/org/spearedrice/asianmod/enchantment/ModEnchantments.java
+++ src/main/java/org/spearedrice/asianmod/enchantment/ModEnchantments.java
@@ -5,16 +5,19 @@ import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.enchantment.Enchantment;
 
+import org.spearedrice.asianmod.AsianMod;
+
+// take from tutorial since i actually want it
 public class ModEnchantments {
+    // :::register-enchantment
+    public static final ResourceKey<Enchantment> THUNDERING = key("thundering");
+    // :::register-enchantment
+    public static final ResourceKey<Enchantment> REPULSION_CURSE = key("repulsion_curse");
 
-    public static final ResourceKey<Enchantment> RITUALISTIC_SACRIFICE =
-            key("ritualistic_sacrifice");
-
-    public static final ResourceKey<Enchantment> BURDENED_SPIRIT =
-            key("burdened_spirit");
-
+    // :::key-helper
     private static ResourceKey<Enchantment> key(String path) {
-        Identifier id = Identifier.fromNamespaceAndPath("asianmod", path);
+        Identifier id = Identifier.fromNamespaceAndPath(AsianMod.MOD_ID, path);
         return ResourceKey.create(Registries.ENCHANTMENT, id);
     }
+    // :::key-helper
 }
