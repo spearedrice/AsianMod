a43af98ecb4712068d8981b8550be7af910c9803
spearedrice <225486530+spearedrice@users.noreply.github.com>
2026-05-08 18:59:15 -0400

    The beginning of something great.

---
+++ src/main/java/org/spearedrice/asianmod/AsianModDataGenerator.java
@@ -0,0 +1,11 @@
+package org.spearedrice.asianmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AsianModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
