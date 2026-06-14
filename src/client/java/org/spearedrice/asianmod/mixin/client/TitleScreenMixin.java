b8d24660edfb6413e3e06877e10a7b2060b97d74
spearedrice <225486530+spearedrice@users.noreply.github.com>
2026-06-13 20:29:06 -0400

    stop tracking generated files

--- src/client/java/org/spearedrice/asianmod/mixin/client/TitleScreenMixin.java
+++ src/client/java/org/spearedrice/asianmod/mixin/client/TitleScreenMixin.java
@@ -1,27 +1,13 @@
 package org.spearedrice.asianmod.mixin.client;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.network.chat.Component;
-
-import org.spearedrice.asianmod.rendering.LiveActionRolePlay;
-import org.spearedrice.asianmod.rendering.screens.TuffScreen;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin extends Screen {
     protected TitleScreenMixin(Component title) {
         super(title);
-    }
-
-    @Inject(method = "init", at = @At("TAIL"), cancellable = false)
-    private void addTestWidgets(CallbackInfo ci) {
-        this.addRenderableWidget(Button.builder(Component.nullToEmpty("LARP"), (btn) -> this.minecraft.setScreen(new LiveActionRolePlay())).bounds(5, 5, 60, 20).build());
-        this.addRenderableWidget(Button.builder(Component.nullToEmpty("TuffScreen"), (btn) -> this.minecraft.setScreen(new TuffScreen(Component.empty()))).bounds(5, 5+30, 60, 20).build());
     }
 }
--- src/main/java/org/spearedrice/asianmod/advancement/ModCriteria.java
+++ src/main/java/org/spearedrice/asianmod/advancement/ModCriteria.java
@@ -1,17 +1,14 @@
 package org.spearedrice.asianmod.advancement;
 
 import net.minecraft.advancements.CriteriaTriggers;
-
 import org.spearedrice.asianmod.AsianMod;
 
 public class ModCriteria {
+    public static final UseToolCriterion USE_TOOL = new UseToolCriterion();
+    public static final ParameterizedUseToolCriterion PARAMETERIZED_USE_TOOL = new ParameterizedUseToolCriterion();
 
-    public static final UseToolCriterion USE_TOOL =
-            CriteriaTriggers.register(AsianMod.MOD_ID + ":use_tool", new UseToolCriterion());
-
-    public static final ParameterizedUseToolCriterion PARAMETERIZED_USE_TOOL =
-            CriteriaTriggers.register(AsianMod.MOD_ID + ":parameterized_use_tool", new ParameterizedUseToolCriterion());
-
-    public static void init() {
+    public static void register() {
+        CriteriaTriggers.register(AsianMod.MOD_ID + ":use_tool", USE_TOOL);
+        CriteriaTriggers.register(AsianMod.MOD_ID + ":parameterized_use_tool", PARAMETERIZED_USE_TOOL);
     }
 }
